49d1f3f15baac15b404d9f38e5ce245447ef7c46
aishwarya-1307 <aishwaryagoud2007@gmail.com>
2026-07-10 11:54:25 +0530

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,9 @@
+class FirstJava{
+    public void hello(){
+    System.out.println("Hello");
+    }
+    public static void main(String...args){
+    FirstJava fj = new FirstJava();
+    fj.hello();
+    }
+}
