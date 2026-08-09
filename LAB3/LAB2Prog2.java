47525d476a5613392fa124372212d3f5b752d595
aishwarya-1307 <aishwaryagoud2007@gmail.com>
2026-08-09 10:46:44 +0000

    Add files via upload

---
+++ LAB3/LAB2Prog2.java
@@ -0,0 +1,30 @@
+class LAB2Prog2{
+	public static void main(String...args){
+          byte b=10;
+          short s=234;
+          int i=1234;
+          long l=12345;
+
+          float fl=1234.33f;
+          double d=1234.567;
+          
+          double di =i;
+          int id=(int)di;
+
+          float fb=b;
+          byte bf=(byte)fb;
+           
+          double dl =l;
+          long ld = (long)dl;
+
+          int ib =b;
+          byte bi =(byte)ib;
+
+          int is =s;
+          short si =(short)is;
+
+          long ls =s;
+          short sl =(short)ls;
+    }
+           
+}
